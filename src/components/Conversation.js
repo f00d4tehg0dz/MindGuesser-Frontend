9b240fa6e77ee6915ccce6f4fec41f1dab3c627a
Adrian Chrysanthou <adrianvfx@gmail.com>
2023-03-22 17:35:43 -0400

    change endpoint

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -35,7 +35,7 @@ const Conversation = () => {
         }
         const fullUserInput = `I'm ${selectedType}: ${userInput}`;
         try {
-            const response = await axios.post('https://f00d.me/mindguesserapi/continue-conversation', {
+            const response = await axios.post('https://f00d.me/continue-conversation', {
                 conversationId,
                 userInput: fullUserInput,
             });
